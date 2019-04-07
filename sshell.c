e2818cfd834d505aef1d3bd82dbb1e4ad52536f5
Matthew Ha <mnha@ucdavis.edu>
2019-04-07 00:15:39 -0700

    user input

--- sshell.c
+++ sshell.c
@@ -5,24 +5,57 @@
 #include <sys/wait.h>
 
 const int INIT_CMD_SIZE = 16;
+const int bufferSize = 512;
 
-void inputStrToArr(char* inputStr, char ***toReturn);
+int nextCommand();
+void inputStrToArr(char* inputStr, char*** toReturn);
+char* getInput(char* buffer);
+int processInput(char** command);
 
 int main(int argc, char *argv[]){
-
 	//TODO: Read this from terminal
 	char inputStr[] = "/bin/date -u";
 
-	char **command = NULL;
-	inputStrToArr(inputStr,&command);
+	return nextCommand();
+}
 
-	char *args[] = { command[0], "-u", NULL };
+int nextCommand() {	
+	char* inputBuffer = malloc( sizeof(char) * 512);
+	getInput(inputBuffer);	
+	inputBuffer[strcspn(inputBuffer, "\n")] = 0;
+
+	char **command = NULL;
+	inputStrToArr(inputBuffer,&command);
+	
+	processInput(command);
+
+	nextCommand();
+}
+
+char* getInput(char* buffer) {
+	printf("sshell$ ");
+	fgets(buffer, bufferSize + 1, stdin);
+	return buffer;
+}
+
+int processInput(char ** command) {
+
+	char *args[] = { command[0], *command, NULL };
 	int retval;
 	
 	if (fork() != 0) {
 		/* This is the Parent*/
 		int status;
 		waitpid(-1, &status, 0);
+		//TODO: Doens't print full command
+		fprintf(stderr, "+ completed ");
+
+		fprintf(stderr, "'");
+		for (int i = 0; command[i] != NULL; i++) {
+			if (i > 0) fprintf(stderr, " ");
+			fprintf(stderr, "%s", command[i]);
+		}
+		fprintf(stderr,"' [%i]\n", status);
 	} else {
 		execv(command[0],args);
 		perror("execv");
@@ -30,9 +63,10 @@ int main(int argc, char *argv[]){
 	}
 
 	return EXIT_SUCCESS;
+
 }
 
-void inputStrToArr(char* inputStr, char ***toReturn) {
+void inputStrToArr(char* inputStr, char*** toReturn) {
 	const char *s = " ";
 	int size = INIT_CMD_SIZE;
 
@@ -49,7 +83,7 @@ void inputStrToArr(char* inputStr, char ***toReturn) {
 		//TODO: Test this
 		if (i == ( size-1 )) 
 		{
-			printf("filled %i of %i elements\n", i+1, size);
+			//printf("filled %i of %i elements\n", i+1, size);
 
 			char **newToReturn = realloc(*toReturn, sizeof(char *) * size*2);
 			*toReturn = newToReturn;
